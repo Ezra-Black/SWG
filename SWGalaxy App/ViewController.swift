0a48cf35171b49a3efaf5ba21fe1abb6f75714f4
JosephRogers94 <joerogersss2016@gmail.com>
2020-01-22 19:38:02 -0600

    Initial Commit

---
+++ SWGalaxy App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SWGalaxy App
+//
+//  Created by Joseph Rogers on 1/22/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
