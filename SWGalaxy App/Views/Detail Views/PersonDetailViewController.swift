3aa298d8fd0c8651d773e6fb5410398df27cfd1b
Ezra Black <dev.ezrablack@gmail.com>
2020-02-18 00:29:10 -0600

    Added BoilerPl8 for constraints in Detail View

--- SWGalaxy App/Views/Detail Views/PersonDetailViewController.swift
+++ SWGalaxy App/Views/Detail Views/PersonDetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class PersonDetailViewController: UIViewController {
     
-    //MARK: Properties
+    //MARK: - Outlets
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var genderLabel: UILabel!
@@ -18,13 +18,20 @@ class PersonDetailViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var massLabel: UILabel!
     
+    //MARK: - Properties
+    
     var person: Person?
+    
+    //MARK: - Functions
+    
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
-        // Do any additional setup after loading the view.
+        setupSubViews()
     }
+    
     private func updateViews() {
         guard let person = person else { return }
         nameLabel.text = "Name: \(person.name)"
@@ -33,4 +40,44 @@ class PersonDetailViewController: UIViewController {
         heightLabel.text = "Height: \(person.height)"
         massLabel.text = "Mass(kg): \(person.mass)"
     }
+    
+    //MARK: - SubViews
+    
+    private func setupSubViews() {
+        
+        let nameLabel = UILabel()
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        
+        //MARK: - Name Label Constraints
+        
+        nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -20).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: -8).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 8).isActive = true
+        self.nameLabel = nameLabel
+        
+        let genderLabel = UILabel()
+        genderLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(genderLabel)
+        self.genderLabel = genderLabel
+        
+        let birthYearLabel = UILabel()
+        birthYearLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(birthYearLabel)
+        self.birthYearLabel = birthYearLabel
+        
+        let heightLabel = UILabel()
+        heightLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(heightLabel)
+        self.heightLabel = heightLabel
+        
+        let massLabel = UILabel()
+        massLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(massLabel)
+        self.massLabel = massLabel
+    }
+    
+    
+    
 }
